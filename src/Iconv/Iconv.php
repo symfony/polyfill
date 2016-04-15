4836c6050fccaa133f1721748e7eb32335a6ae58
Nicolas Grekas <nicolas.grekas@gmail.com>
2016-04-15 09:04:29 +0200

    Use plain PHP for data maps to benefit from OPcache on PHP 5.6+

--- src/Iconv/Iconv.php
+++ src/Iconv/Iconv.php
@@ -716,8 +716,8 @@ final class Iconv
 
     private static function getData($file)
     {
-        if (file_exists($file = __DIR__.'/Resources/charset/'.$file.'.ser')) {
-            return unserialize(file_get_contents($file));
+        if (file_exists($file = __DIR__.'/Resources/charset/'.$file.'.php')) {
+            return require $file;
         }
 
         return false;
