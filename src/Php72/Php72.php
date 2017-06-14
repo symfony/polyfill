06aee59eb913b82b9d23942113b0a9926ba07be1
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-06-14 09:46:01 +0200

    update after php-src implementation changed

--- src/Php72/Php72.php
+++ src/Php72/Php72.php
@@ -69,7 +69,7 @@ final class Php72
         }
 
         $map = array(
-            'Darwin' => 'Mac',
+            'Darwin' => 'Darwin',
             'DragonFly' => 'BSD',
             'FreeBSD' => 'BSD',
             'NetBSD' => 'BSD',
