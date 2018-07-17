7ad6313663122c642a4af4866b1ba2fcc2532653
Pierre du Plessis <pierre@pcservice.co.za>
2018-07-17 14:02:20 +0200

    PHP 7.3: Add array_key_first and array_key_last functions

--- tests/Php73/Php73Test.php
+++ tests/Php73/Php73Test.php
@@ -15,6 +15,7 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @author Gabriel Caruso <carusogabriel34@gmail.com>
+ * @author Pierre du Plessis <pdples@gmail.com>
  */
 class Php73Test extends TestCase
 {
@@ -86,5 +87,79 @@ class Php73Test extends TestCase
 
         $this->assertGreaterThanOrEqual(1, $hrtime2[0] - $hrtime[0]);
         $this->assertGreaterThanOrEqual(0, $hrtime2[1] - $hrtime[1]);
+    }
+
+    /**
+     * @dataProvider arrayKeyFirstDataProvider
+     */
+    public function testArrayKeyFirst($expected, array $array)
+    {
+        $this->assertSame($expected, array_key_first($array));
+    }
+
+    public function testArrayKeyFirstVariation()
+    {
+        $array = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        $this->assertSame(1, current($array));
+        $this->assertSame(2, next($array));
+        $this->assertSame(0, array_key_first($array));
+        $this->assertSame(2, current($array));
+    }
+
+    /**
+     * @dataProvider arrayKeyLastDataProvider
+     */
+    public function testArrayLastFirst($expected, array $array)
+    {
+        $this->assertSame($expected, array_key_last($array));
+    }
+
+    public function testArrayKeyLastVariation()
+    {
+        $array = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        $this->assertSame(1, current($array));
+        $this->assertSame(2, next($array));
+        $this->assertSame(8, array_key_last($array));
+        $this->assertSame(2, current($array));
+    }
+
+    public function arrayKeyFirstDataProvider()
+    {
+        return array(
+            array(null,  array()),
+            array(0,     array(1, 2, 3, 4, 5, 6, 7, 8, 9)),
+            array(0,     array('One', '_Two', 'Three', 'Four', 'Five')),
+            array(0,     array(6, 'six', 7, 'seven', 8, 'eight', 9, 'nine')),
+            array('a',   array('a' => 'aaa', 'A' => 'AAA', 'c' => 'ccc', 'd' => 'ddd', 'e' => 'eee')),
+            array(1,     array('1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five')),
+            array(1,     array(1 => 'one', 2 => 'two', 3 => 7, 4 => 'four', 5 => 'five')),
+            array('f',   array('f' => 'fff', '1' => 'one', 4 => 6, '' => 'blank', 2.4 => 'float', 'F' => 'FFF', 'blank' => '', 3.7 => 3.7, 5.4 => 7, 6 => 8.6, '5' => 'Five', '4name' => 'jonny', 'a' => NULL, NULL => 3)),
+            array(0,     array(12, 'name', 'age', '45')),
+            array(0,     array(array('oNe', 'tWo', 4), array(10, 20, 30, 40, 50), array())),
+            array('one', array('one' => 1, 'one' => 2, 'three' => 3, 3, 4, 3 => 33, 4 => 44, 5, 6, 5.4 => 54, 5.7 => 57, '5.4' => 554, '5.7' => 557)),
+            array(0,     array('foo')),
+            array(1,     array(1 => '42'))
+        );
+    }
+
+    public function arrayKeyLastDataProvider()
+    {
+        return array(
+            array(null,  array()),
+            array(8,     array(1, 2, 3, 4, 5, 6, 7, 8, 9)),
+            array(4,     array('One', '_Two', 'Three', 'Four', 'Five')),
+            array(7,     array(6, 'six', 7, 'seven', 8, 'eight', 9, 'nine')),
+            array('e',   array('a' => 'aaa', 'A' => 'AAA', 'c' => 'ccc', 'd' => 'ddd', 'e' => 'eee')),
+            array(5,     array('1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five')),
+            array(5,     array(1 => 'one', 2 => 'two', 3 => 7, 4 => 'four', 5 => 'five')),
+            array('a',   array('f' => 'fff', '1' => 'one', 4 => 6, '' => 'blank', 2.4 => 'float', 'F' => 'FFF', 'blank' => '', 3.7 => 3.7, 5.4 => 7, 6 => 8.6, '5' => 'Five', '4name' => 'jonny', 'a' => NULL, NULL => 3)),
+            array(3,     array(12, 'name', 'age', '45')),
+            array(2,     array(array('oNe', 'tWo', 4), array(10, 20, 30, 40, 50), array())),
+            array('5.7', array('one' => 1, 'one' => 2, 'three' => 3, 3, 4, 3 => 33, 4 => 44, 5, 6, 5.4 => 54, 5.7 => 57, '5.4' => 554, '5.7' => 557)),
+            array(0,     array('foo')),
+            array(1,     array(1 => '42'))
+        );
     }
 }
