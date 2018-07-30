b0b38575100a39c9f5039ebef7c8f74902921fc2
Yanni <peter.yanni@mac-rktba-00002.fritz.box>
2018-07-30 20:50:32 +0200

    Add JSON errors which supported by php55

--- src/Php55/Php55.php
+++ src/Php55/Php55.php
@@ -30,6 +30,9 @@ final class Php55
             case JSON_ERROR_CTRL_CHAR: return 'Control character error, possibly incorrectly encoded';
             case JSON_ERROR_SYNTAX: return 'Syntax error';
             case JSON_ERROR_UTF8: return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+            case JSON_ERROR_RECURSION: return 'One or more recursive references in the value to be encoded';
+            case JSON_ERROR_INF_OR_NAN: return 'One or more NAN or INF values in the value to be encoded';
+            case JSON_ERROR_UNSUPPORTED_TYPE: return 'A value of a type that cannot be encoded was given';
             default: return 'Unknown error';
         }
     }
