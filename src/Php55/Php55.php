b9d1f2eb01393a5a2535a0b87164b9063e962b51
Yanni <peter.yanni@mac-rktba-00002.fritz.box>
2018-08-05 15:51:30 +0200

    update the messages to match the PHP source code

--- src/Php55/Php55.php
+++ src/Php55/Php55.php
@@ -30,9 +30,9 @@ final class Php55
             case JSON_ERROR_CTRL_CHAR: return 'Control character error, possibly incorrectly encoded';
             case JSON_ERROR_SYNTAX: return 'Syntax error';
             case JSON_ERROR_UTF8: return 'Malformed UTF-8 characters, possibly incorrectly encoded';
-            case JSON_ERROR_RECURSION: return 'One or more recursive references in the value to be encoded';
-            case JSON_ERROR_INF_OR_NAN: return 'One or more NAN or INF values in the value to be encoded';
-            case JSON_ERROR_UNSUPPORTED_TYPE: return 'A value of a type that cannot be encoded was given';
+            case JSON_ERROR_RECURSION: return 'Recursion detected';
+            case JSON_ERROR_INF_OR_NAN: return 'Inf and NaN cannot be JSON encoded';
+            case JSON_ERROR_UNSUPPORTED_TYPE: return 'Type is not supported';
             default: return 'Unknown error';
         }
     }
