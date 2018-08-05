61aa24cfb90c30a5e143143b6ce66c20f39008ac
Nicolas Grekas <nicolas.grekas@gmail.com>
2018-08-05 15:13:47 +0200

    skip ResourceBundle test on big endian systems

--- tests/Php73/Php73Test.php
+++ tests/Php73/Php73Test.php
@@ -24,8 +24,12 @@ class Php73Test extends TestCase
         $this->assertTrue(is_countable(new \ArrayIterator(array('foo', 'bar', 'baz'))));
         $this->assertTrue(is_countable(new \ArrayIterator()));
         $this->assertTrue(is_countable(new \SimpleXMLElement('<foo><bar/><bar/><bar/></foo>')));
-        $this->assertTrue(is_countable(\ResourceBundle::create('en', __DIR__.'/fixtures')));
         $this->assertFalse(is_countable(new \stdClass()));
+
+        $endianBytes = unpack('S', "\x01\x00");
+        if (1 === $endianBytes[1]) { // skip on big endian systems: the fixture is only for little endian ones
+            $this->assertTrue(is_countable(\ResourceBundle::create('en', __DIR__.'/fixtures')));
+        }
     }
 
     /**
