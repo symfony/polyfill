36fc3685f7d914167c557f524867b7d1befab520
Nicolas Grekas <nicolas.grekas@gmail.com>
2021-01-06 10:53:20 +0100

    Add polyfill-php81

---
+++ src/Php81/README.md
@@ -0,0 +1,14 @@
+Symfony Polyfill / Php80
+========================
+
+This component provides features added to PHP 8.1 core:
+
+- [`array_is_list`](https://php.net/array_is_list)
+
+More information can be found in the
+[main Polyfill README](https://github.com/symfony/polyfill/blob/master/README.md).
+
+License
+=======
+
+This library is released under the [MIT license](LICENSE).
---
+++ tests/Php81/Php81Test.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Polyfill\Tests\Php81;
+
+use PHPUnit\Framework\TestCase;
+
+class Php81Test extends TestCase
+{
+    /**
+     * @covers \Symfony\Polyfill\Php81\Php81::array_is_list
+     */
+    public function testArrayIsList()
+    {
+        $this->assertTrue(array_is_list([]));
+        $this->assertTrue(array_is_list([\NAN, 'foo', 123]));
+        $this->assertFalse(array_is_list([1 => 'a', 0 => 'b']));
+        $this->assertFalse(array_is_list(['a' => 'b']));
+        $this->assertFalse(array_is_list([0 => 'a', 2 => 'b']));
+        $this->assertFalse(array_is_list([1 => 'a', 2 => 'b']));
+    }
+}
