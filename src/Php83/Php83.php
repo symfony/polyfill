7ccd4161aa3bc4586aa0e9d55ec9ccbe7819fc82
Ion Bazan <ion.bazan@gmail.com>
2023-07-06 12:01:07 +0700

    [PHP 8.3] Polyfill mb_str_pad()

--- src/Php83/Php83.php
+++ src/Php83/Php83.php
@@ -38,4 +38,48 @@ final class Php83
 
         return \JSON_ERROR_NONE === json_last_error();
     }
+
+    public static function mb_str_pad(string $string, int $length, string $pad_string = ' ', int $pad_type = \STR_PAD_RIGHT, string $encoding = null): string
+    {
+        if (!\in_array($pad_type, [\STR_PAD_RIGHT, \STR_PAD_LEFT, \STR_PAD_BOTH], true)) {
+            throw new \ValueError('mb_str_pad(): Argument #4 ($pad_type) must be STR_PAD_LEFT, STR_PAD_RIGHT, or STR_PAD_BOTH');
+        }
+
+        if (null === $encoding) {
+            $encoding = mb_internal_encoding();
+        }
+
+        try {
+            $validEncoding = @mb_check_encoding('', $encoding);
+        } catch (\ValueError $e) {
+            throw new \ValueError(sprintf('mb_str_pad(): Argument #5 ($encoding) must be a valid encoding, "%s" given', $encoding));
+        }
+
+        // BC for PHP 7.3 and lower
+        if (!$validEncoding) {
+            throw new \ValueError(sprintf('mb_str_pad(): Argument #5 ($encoding) must be a valid encoding, "%s" given', $encoding));
+        }
+
+        if (mb_strlen($pad_string, $encoding) <= 0) {
+            throw new \ValueError('mb_str_pad(): Argument #3 ($pad_string) must be a non-empty string');
+        }
+
+        $paddingRequired = $length - mb_strlen($string, $encoding);
+
+        if ($paddingRequired < 1) {
+            return $string;
+        }
+
+        switch ($pad_type) {
+            case \STR_PAD_LEFT:
+                return mb_substr(str_repeat($pad_string, $paddingRequired), 0, $paddingRequired, $encoding).$string;
+            case \STR_PAD_RIGHT:
+                return $string.mb_substr(str_repeat($pad_string, $paddingRequired), 0, $paddingRequired, $encoding);
+            default:
+                $leftPaddingLength = floor($paddingRequired / 2);
+                $rightPaddingLength = $paddingRequired - $leftPaddingLength;
+
+                return mb_substr(str_repeat($pad_string, $leftPaddingLength), 0, $leftPaddingLength, $encoding).$string.mb_substr(str_repeat($pad_string, $rightPaddingLength), 0, $rightPaddingLength, $encoding);
+        }
+    }
 }
--- tests/Mbstring/MbstringTest.php
+++ tests/Mbstring/MbstringTest.php
@@ -626,4 +626,104 @@ class MbstringTest extends TestCase
         $this->assertSame(sprintf('Test: %s', base64_decode('7/Du4uXw6uA=')), mb_decode_mimeheader('Test: =?windows-1251?B?7/Du4uXw6uA=?='));
         $this->assertTrue(mb_internal_encoding('utf8'));
     }
+
+    /**
+     * @covers \Symfony\Polyfill\Mbstring\Mbstring::mb_str_pad
+     *
+     * @dataProvider paddingStringProvider
+     * @dataProvider paddingEmojiProvider
+     * @dataProvider paddingEncodingProvider
+     */
+    public function testMbStrPad(string $expectedResult, string $string, int $length, string $padString, int $padType, string $encoding = null): void
+    {
+        if ('UTF-32' === $encoding && \PHP_VERSION_ID < 73000) {
+            $this->markTestSkipped('PHP < 7.3 doesn\'t handle UTF-32 encoding properly');
+        }
+
+        $this->assertSame($expectedResult, mb_convert_encoding(mb_str_pad($string, $length, $padString, $padType, $encoding), 'UTF-8', $encoding ?? mb_internal_encoding()));
+    }
+
+    /**
+     * @covers \Symfony\Polyfill\Mbstring\Mbstring::mb_str_pad
+     *
+     * @dataProvider mbStrPadInvalidArgumentsProvider
+     */
+    public function testMbStrPadInvalidArguments(string $expectedError, string $string, int $length, string $padString, int $padType, string $encoding = null): void
+    {
+        $this->expectException(\ValueError::class);
+        $this->expectErrorMessage($expectedError);
+
+        mb_str_pad($string, $length, $padString, $padType, $encoding);
+    }
+
+    public static function paddingStringProvider(): iterable
+    {
+        // Simple ASCII strings
+        yield ['+Hello+', 'Hello', 7, '+-', \STR_PAD_BOTH];
+        yield ['+-World+-+', 'World', 10, '+-', \STR_PAD_BOTH];
+        yield ['+-Hello', 'Hello', 7, '+-', \STR_PAD_LEFT];
+        yield ['+-+-+World', 'World', 10, '+-', \STR_PAD_LEFT];
+        yield ['Hello+-', 'Hello', 7, '+-', \STR_PAD_RIGHT];
+        yield ['World+-+-+', 'World', 10, '+-', \STR_PAD_RIGHT];
+        // Edge cases pad length
+        yield ['▶▶', '▶▶', 2, ' ', \STR_PAD_BOTH];
+        yield ['▶▶', '▶▶', 1, ' ', \STR_PAD_BOTH];
+        yield ['▶▶', '▶▶', 0, ' ', \STR_PAD_BOTH];
+        yield ['▶▶', '▶▶', -1, ' ', \STR_PAD_BOTH];
+        // Empty input string
+        yield ['  ', '', 2, ' ', \STR_PAD_BOTH];
+        yield [' ', '', 1, ' ', \STR_PAD_BOTH];
+        yield ['', '', 0, ' ', \STR_PAD_BOTH];
+        yield ['', '', -1, ' ', \STR_PAD_BOTH];
+        // Default argument
+        yield ['▶▶    ', '▶▶', 6, ' ', \STR_PAD_RIGHT];
+        yield ['    ▶▶', '▶▶', 6, ' ', \STR_PAD_LEFT];
+        yield ['  ▶▶  ', '▶▶', 6, ' ', \STR_PAD_BOTH];
+    }
+
+    public static function paddingEmojiProvider(): iterable
+    {
+        // UTF-8 Emojis
+        yield ['▶▶❤❓❇❤', '▶▶', 6, '❤❓❇', \STR_PAD_RIGHT];
+        yield ['❤❓❇❤▶▶', '▶▶', 6, '❤❓❇', \STR_PAD_LEFT];
+        yield ['❤❓▶▶❤❓', '▶▶', 6, '❤❓❇', \STR_PAD_BOTH];
+        yield ['▶▶❤❓❇', '▶▶', 5, '❤❓❇', \STR_PAD_RIGHT];
+        yield ['❤❓❇▶▶', '▶▶', 5, '❤❓❇', \STR_PAD_LEFT];
+        yield ['❤▶▶❤❓', '▶▶', 5, '❤❓❇', \STR_PAD_BOTH];
+        yield ['▶▶❤❓', '▶▶', 4, '❤❓❇', \STR_PAD_RIGHT];
+        yield ['❤❓▶▶', '▶▶', 4, '❤❓❇', \STR_PAD_LEFT];
+        yield ['❤▶▶❤', '▶▶', 4, '❤❓❇', \STR_PAD_BOTH];
+        yield ['▶▶❤', '▶▶', 3, '❤❓❇', \STR_PAD_RIGHT];
+        yield ['❤▶▶', '▶▶', 3, '❤❓❇', \STR_PAD_LEFT];
+        yield ['▶▶❤', '▶▶', 3, '❤❓❇', \STR_PAD_BOTH];
+
+        for ($i = 2; $i >= 0; --$i) {
+            yield ['▶▶', '▶▶', $i, '❤❓❇', \STR_PAD_RIGHT];
+            yield ['▶▶', '▶▶', $i, '❤❓❇', \STR_PAD_LEFT];
+            yield ['▶▶', '▶▶', $i, '❤❓❇', \STR_PAD_BOTH];
+        }
+    }
+
+    public static function paddingEncodingProvider(): iterable
+    {
+        $string = 'Σὲ γνωρίζω ἀπὸ τὴν κόψη Зарегистрируйтесь';
+
+        foreach (['UTF-8', 'UTF-32', 'UTF-7'] as $encoding) {
+            $input = mb_convert_encoding($string, $encoding, 'UTF-8');
+            $padStr = mb_convert_encoding('▶▶', $encoding, 'UTF-8');
+
+            yield ['Σὲ γνωρίζω ἀπὸ τὴν κόψη Зарегистрируйтесь▶▶▶', $input, 44, $padStr, \STR_PAD_RIGHT, $encoding];
+            yield ['▶▶▶Σὲ γνωρίζω ἀπὸ τὴν κόψη Зарегистрируйтесь', $input, 44, $padStr, \STR_PAD_LEFT, $encoding];
+            yield ['▶Σὲ γνωρίζω ἀπὸ τὴν κόψη Зарегистрируйтесь▶▶', $input, 44, $padStr, \STR_PAD_BOTH, $encoding];
+        }
+    }
+
+    public static function mbStrPadInvalidArgumentsProvider(): iterable
+    {
+        yield ['mb_str_pad(): Argument #3 ($pad_string) must be a non-empty string', '▶▶', 6, '', \STR_PAD_RIGHT];
+        yield ['mb_str_pad(): Argument #3 ($pad_string) must be a non-empty string', '▶▶', 6, '', \STR_PAD_LEFT];
+        yield ['mb_str_pad(): Argument #3 ($pad_string) must be a non-empty string', '▶▶', 6, '', \STR_PAD_BOTH];
+        yield ['mb_str_pad(): Argument #4 ($pad_type) must be STR_PAD_LEFT, STR_PAD_RIGHT, or STR_PAD_BOTH', '▶▶', 6, ' ', 123456];
+        yield ['mb_str_pad(): Argument #5 ($encoding) must be a valid encoding, "unexisting" given', '▶▶', 6, ' ', \STR_PAD_BOTH, 'unexisting'];
+    }
 }
