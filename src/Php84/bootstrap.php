f11f4fd5c394a62288c0f59dea53ebea0ba70eec
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-05-25 08:33:33 +0200

    Improve type declarations fo some mbstring polyfill

--- src/Php84/bootstrap.php
+++ src/Php84/bootstrap.php
@@ -51,22 +51,27 @@ if (\PHP_VERSION_ID >= 80000) {
 
 if (extension_loaded('mbstring')) {
     if (!function_exists('mb_ucfirst')) {
+        /** @return string|false */
         function mb_ucfirst(string $string, ?string $encoding = null) { return p\Php84::mb_ucfirst($string, $encoding); }
     }
 
     if (!function_exists('mb_lcfirst')) {
+        /** @return string|false */
         function mb_lcfirst(string $string, ?string $encoding = null) { return p\Php84::mb_lcfirst($string, $encoding); }
     }
 
     if (!function_exists('mb_trim')) {
+        /** @return string|false */
         function mb_trim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Php84::mb_trim($string, $characters, $encoding); }
     }
 
     if (!function_exists('mb_ltrim')) {
+        /** @return string|false */
         function mb_ltrim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Php84::mb_ltrim($string, $characters, $encoding); }
     }
 
     if (!function_exists('mb_rtrim')) {
+        /** @return string|false */
         function mb_rtrim(string $string, ?string $characters = null, ?string $encoding = null) { return p\Php84::mb_rtrim($string, $characters, $encoding); }
     }
 }
