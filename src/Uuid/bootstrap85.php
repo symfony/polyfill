4b3929c3722f8b14c400576047c7864561e77754
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-05-25 09:16:34 +0200

    [Uuid] Mark UUID_TYPE_DCE and UUID_TYPE_NAME as deprecated on PHP 8.5+
    Loads a dedicated bootstrap85.php that declares those two aliases via
    const so the #[Deprecated] attribute can be attached. The file is only
    required on PHP 8.5+ to keep older parsers from rejecting the syntax.

---
+++ src/Uuid/bootstrap85.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+#[Deprecated(message: 'use UUID_TYPE_RANDOM instead')]
+const UUID_TYPE_DCE = 4;
+
+#[Deprecated(message: 'use UUID_TYPE_TIME instead')]
+const UUID_TYPE_NAME = 1;
